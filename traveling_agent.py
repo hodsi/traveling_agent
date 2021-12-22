76f4e28df50e6b3d724f324e32d39c7bad697ff0
Hod Sinay <sinayhod@gmail.com>
2021-12-23 00:43:40 +0200

    added the fusion of the circles

--- traveling_agent.py
+++ traveling_agent.py
@@ -1,5 +1,6 @@
+import itertools
 from copy import deepcopy
-from typing import List, Optional, Iterator
+from typing import List, Optional, Iterator, Tuple
 
 from entities.line import Line
 from entities.route import Route
@@ -96,7 +97,7 @@ def falsify_line(matrix: WeightsMatrix, line: Line):
 
 def get_covering_lines(matrix: WeightsMatrix) -> List[Line]:
     covering_matrix = deepcopy(matrix)
-    convert_matrix_for_circle_findings(covering_matrix, False)
+    convert_matrix_for_circle_findings(covering_matrix, falsify_diagonal=False)
     covering_lines = []
     while True in covering_matrix:
         line = get_covering_line(covering_matrix)
@@ -123,10 +124,65 @@ def optimize_matrix_placement(matrix: WeightsMatrix):
                     matrix.matrix_of_weights[i][j] -= subtraction_number
 
 
+def calc_fusion_cost(
+        matrix: WeightsMatrix, circle1: Route, circle2: Route, source1_index: int, source2_index: int
+) -> int:
+    source1 = circle1.vertexes[source1_index]
+    source2 = circle2.vertexes[source2_index]
+    destination1 = circle1.vertexes[(source1_index + 1) % len(circle1.vertexes)]
+    destination2 = circle2.vertexes[(source2_index + 1) % len(circle2.vertexes)]
+    return (
+            matrix.get_weight(source1, destination2) + matrix.get_weight(source2, destination1) -
+            matrix.get_weight(source1, destination1) - matrix.get_weight(source2, destination2)
+    )
+
+
+def calc_minimum_circles_fusion(matrix: WeightsMatrix, circle1: Route, circle2: Route) -> Tuple[Tuple[int, int], int]:
+    vertexes_combinations = itertools.product(range(len(circle1.vertexes)), range(len(circle2.vertexes)))
+    min_indexes = next(vertexes_combinations)
+    min_cost = calc_fusion_cost(matrix, circle1, circle2, min_indexes[0], min_indexes[1])
+    for indexes in vertexes_combinations:
+        cost = calc_fusion_cost(matrix, circle1, circle2, indexes[0], indexes[1])
+        if cost < min_cost:
+            min_indexes = indexes
+            min_cost = cost
+    return min_indexes, min_cost
+
+
+def fuse_two_circles(circle1: Route, circle2: Route, source1_index: int, source2_index: int) -> Route:
+    return Route(
+        circle1.vertexes[:source1_index + 1] +
+        circle2.vertexes[source2_index + 1:] +
+        circle2.vertexes[:source2_index + 1:] +
+        circle1.vertexes[source1_index + 1:],
+        is_circle=True
+    )
+
+
+def fuse_circles(matrix: WeightsMatrix, circles: List[Route]) -> Route:
+    circles = deepcopy(circles)
+    while len(circles) > 1:
+        circle_combinations = itertools.combinations(circles, 2)
+        min_circles = next(circle_combinations)
+        min_indexes, min_cost = calc_minimum_circles_fusion(matrix, min_circles[0], min_circles[1])
+        for circle1, circle2 in circle_combinations:
+            indexes, cost = calc_minimum_circles_fusion(matrix, circle1, circle2)
+            if cost < min_cost:
+                min_circles = circle1, circle2
+                min_indexes = indexes
+                min_cost = cost
+        fused_circle = fuse_two_circles(min_circles[0], min_circles[1], min_indexes[0], min_indexes[1])
+        circles.remove(min_circles[0])
+        circles.remove(min_circles[1])
+        circles.append(fused_circle)
+    return circles.pop()
+
+
 def calc_approximate_route(weights_matrix: WeightsMatrix):
     subtracted_matrix = calc_subtracted_matrix(weights_matrix)
     optimize_matrix_placement(subtracted_matrix)
     convert_matrix_for_circle_findings(subtracted_matrix)
     possible_circles = [*find_circles(subtracted_matrix)]
-    circles_to_take = min(possible_circles, key=(lambda circle: len(circle)))
-    print(circles_to_take)
+    circles_to_take = min(possible_circles, key=len)
+    fused_circle = fuse_circles(weights_matrix, circles_to_take)
+    print(f'the fused circle is {fused_circle}, and its weight is {weights_matrix.get_route_weight(fused_circle)}')
